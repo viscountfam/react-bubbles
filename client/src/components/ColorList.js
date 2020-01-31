b8c1d1d6c48934565bbdcb2ddbe754a3afde8029
viscountfam <V3ctor42!>
2020-01-31 12:09:52 -0500

    more functionality added

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import {axiosWithAuth} from "../utils/axiosWithAuth"
 const initialColor = {
   color: "",
   code: { hex: "" }
@@ -18,13 +18,30 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`colors/${colorToEdit.id}`, colorToEdit)
+        .then(res => {
+          console.log("this is the response from the color to edit put request", res)
+          axiosWithAuth()
+            .get(`colors`)
+              .then(res => {
+                console.log("Updated color list", res)
+                updateColors(res.data)
+              })
+        })
+        .catch(err => {
+          console.log("An error occurred while trying to update the color", err)
+        })
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+        .delete(`colors/${color.id}`)
+          .then(res => {
+            console.log(`The color ${color.color} was successfully deleted`, res)
+          })
+          updateColors( colors.filter(colors =>
+            colors.id !== color.id))
   };
 
   return (
